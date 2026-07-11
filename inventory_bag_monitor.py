ad2439809eaab7adc41e18c79ab5985d5fa9127e
Chiny <119402252+chinhtran13@users.noreply.github.com>
2026-07-11 11:40:01 +0700

    chore: bump CURRENT_VERSION to v1.0.1

--- inventory_bag_monitor.py
+++ inventory_bag_monitor.py
@@ -13,7 +13,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from tkinter import messagebox, ttk
 
-CURRENT_VERSION = "v1.0.0"
+CURRENT_VERSION = "v1.0.1"
 GITHUB_OWNER = "chinhtran13"
 GITHUB_REPO = "TBH_Tool"
 
